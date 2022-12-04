d39ec01faf45ab6ce54fae2c0726bf06b9a750ac
Nikita <nikita@oden.com>
2022-12-04 22:40:22 +0300

    Huck up buttons to JS and make them change colors

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
+const ACTIVATED_BACKGROUND_COLOR = '#333333';
+const ACTIVATED_TEXT_COLOR = '#e2e2e2';
+const DEACTIVATED_BACKGROUND_COLOR = 'unset'
+const DEACTIVATED_TEXT_COLOR = '#333333'
+
 let canvas = document.querySelector('.canvas');
+let pickedColor = document.querySelector('#picked-color');
+let rainbow = document.querySelector('#rainbow');
+let erase = document.querySelector('#erase');
+let clear = document.querySelector('#clear');
+
 let gridSize = 16;
 let mousedown = false;
+let currentMode = 'pickedColor';
 
+pickedColor.addEventListener('click', () => setMode('pickedColor'));
+rainbow.addEventListener('click', () => setMode('rainbow'));
+erase.addEventListener('click', () => setMode('erase'));
+//clear.addEventListener('click', reloadGrid)
 document.addEventListener('mousedown', () => mousedown = true)
 document.addEventListener('mouseup', () => mousedown = false)
 
@@ -16,6 +31,37 @@ for (let i = 0; i < gridSize**2; i++) {
 
 function changeColor(e){
     if(mousedown){
-        e.target.style.backgroundColor = 'red';
+        e.target.style.backgroundColor = pickedColor.value;
+    }
+}
+
+function setMode(newMode){
+    activateButton(newMode);
+    currentMode = newMode;
+}
+
+function activateButton(newMode){
+    switch (currentMode) {
+        case 'pickedColor':
+            pickedColor.classList.remove('active');
+            break;
+        case 'rainbow':
+            rainbow.classList.remove('active');
+            break;
+        case 'erase':
+            erase.classList.remove('active');
+            break;
+    }
+
+    switch(newMode) {
+        case 'pickedColor':
+            pickedColor.classList.add('active');
+            break;
+        case 'rainbow':
+            rainbow.classList.add('active');
+            break;
+        case 'erase':
+            erase.classList.add('active');
+            break;
     }
 }
