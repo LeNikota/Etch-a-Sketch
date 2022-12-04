1ef8ae1fdbe3a30657064726b6b66d93e739fe19
Nikita <nikita@oden.com>
2022-12-04 21:54:17 +0300

    Add the canvas functionality

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 let canvas = document.querySelector('.canvas');
 let gridSize = 16;
+let mousedown = false;
+
+document.addEventListener('mousedown', () => mousedown = true)
+document.addEventListener('mouseup', () => mousedown = false)
 
 
-for (let i = 0; i < gridSize; i++) {
+for (let i = 0; i < gridSize**2; i++) {
     gridBox = document.createElement('div');
     gridBox.setAttribute('draggable', 'false');
     gridBox.classList.add('grid-item');
+    gridBox.addEventListener('mouseover', changeColor);
     canvas.appendChild(gridBox);
+}
+
+function changeColor(e){
+    if(mousedown){
+        e.target.style.backgroundColor = 'red';
+    }
 }
